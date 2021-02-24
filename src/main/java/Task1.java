a325f6969e09abfbda92cf628229404948319862
Ruslan Mukashev <rmukashev@mail.ru>
2021-02-24 12:18:57 +0600

    new solution

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -2,12 +2,8 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите Ваше имя:");
-        String name = scan.nextLine();
-        scan.close();
-        System.out.println("Привет, " + name + "!!!");
-
+        for (String name: args) {
+            System.out.println("Hello, " + name + "!!!");
+        }
     }
-
 }
