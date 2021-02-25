34140313b7d8c20d13f9d48999827f2b1313e0a8
Ruslan Mukashev <rmukashev@mail.ru>
2021-02-26 01:02:26 +0600

    add task5 solution

---
+++ src/main/java/Task5.java
@@ -0,0 +1,20 @@
+public class Task5 {
+    public static void main(String[] args) {
+        int numberOfMonth = Integer.parseInt(args[0]);
+        switch (numberOfMonth) {
+            case 1 -> System.out.println("January");
+            case 2 -> System.out.println("February");
+            case 3 -> System.out.println("March");
+            case 4 -> System.out.println("April");
+            case 5 -> System.out.println("May");
+            case 6 -> System.out.println("June");
+            case 7 -> System.out.println("July");
+            case 8 -> System.out.println("August");
+            case 9 -> System.out.println("September");
+            case 10 -> System.out.println("October");
+            case 11 -> System.out.println("November");
+            case 12 -> System.out.println("December");
+            default -> System.out.println("Please enter a number from 1 to 12");
+        }
+    }
+}
